01747659ee0ab950b1af338e4c4096ff8ef28de4
Allan <allanvillatoro88@gmail.com>
2025-03-08 17:31:40 -0600

    update datasource type

--- src/types/datasource.d.ts
+++ src/types/datasource.d.ts
@@ -1,3 +1,3 @@
 import { DataSource } from 'typeorm';
 
-export type IDataSource = DataSource | 'Other'; //Add any other data source type using | as needed
+export type IDataSource = DataSource; //Add any other data source type using | as needed
