854e493955b4a70026aebe06702b28409657a8dc
Allan <allanvillatoro88@gmail.com>
2025-03-08 13:56:20 -0600

    add fixtures

--- tests/fixtures/test.movie.ts
+++ tests/fixtures/test.movie.ts
@@ -1,4 +1,5 @@
 import { IMovie } from '../../src/types/movie';
+import { v7 as uuidv7 } from 'uuid';
 
 export const testMovieId = 'movie-id';
 export const testMovie: Partial<IMovie> = {
@@ -18,5 +19,50 @@ export const testMovieList: Partial<IMovie>[] = [
     title: 'Taxi Driver',
     year: 1976,
     country: 'USA',
+  },
+];
+
+export const ratedMovieList: Partial<IMovie>[] = [
+  {
+    movieId: uuidv7(),
+    title: 'The Shawshank Redemption',
+    year: 1994,
+    country: 'USA',
+  },
+  {
+    movieId: uuidv7(),
+    title: 'The Godfather',
+    year: 1972,
+    country: 'USA',
+  },
+  {
+    movieId: uuidv7(),
+    title: 'The Dark Knight',
+    year: 2008,
+    country: 'USA',
+  },
+  {
+    movieId: uuidv7(),
+    title: 'Pulp Fiction',
+    year: 1994,
+    country: 'USA',
+  },
+  {
+    movieId: uuidv7(),
+    title: 'Parasite',
+    year: 2019,
+    country: 'South Korea',
+  },
+  {
+    movieId: uuidv7(),
+    title: 'All Quiet on the Western Front',
+    year: 2022,
+    country: 'Germany',
+  },
+  {
+    movieId: uuidv7(),
+    title: 'Mad Max: Fury Road',
+    year: 2015,
+    country: 'Australia',
   },
 ];
