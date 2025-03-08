8af2bdbf46ab500db3938563f77c754528e41450
Allan <allanvillatoro88@gmail.com>
2025-03-08 15:49:31 -0600

    refactor tests

--- tests/integration/routes/movies.routes.spec.ts
+++ tests/integration/routes/movies.routes.spec.ts
@@ -1,15 +1,14 @@
-import {
-  closeTestDatabase,
-  initializeDatabase,
-  truncateTestDatabaseTables,
-} from '../../../src/config/database';
 import { ITopFiveMovie } from '../../../src/types/movie';
 import { ratedMovieList } from '../../fixtures/test.movie';
-import app from './../../../src/app';
-import request from 'supertest';
+import {
+  closeTestDatabase,
+  initializeTestDatabase,
+  truncateTestDatabaseTables,
+} from '../../testDatabase';
+import { mockRequest } from '../../mockRequest';
 
 beforeAll(async () => {
-  await initializeDatabase();
+  await initializeTestDatabase();
   await truncateTestDatabaseTables();
 
   //Add any mocks to external services
@@ -20,11 +19,11 @@ beforeEach(async () => {
 });
 
 afterEach(async () => {
-  //await truncateTables();
+  await truncateTestDatabaseTables();
   jest.clearAllMocks();
   jest.restoreAllMocks();
-  jest.useRealTimers();
 });
+
 afterAll(async () => {
   await closeTestDatabase();
 });
@@ -32,11 +31,14 @@ afterAll(async () => {
 test('topfive', async () => {
   //Add 7 movies
   for (const movie of ratedMovieList) {
-    await request(app).post('/api/movies').send(movie);
+    await mockRequest({ path: '/api/movies', action: 'post', body: movie });
   }
 
   //Assure the 7 movies are added in the database
-  const moviesResponse = await request(app).get('/api/movies');
+  const moviesResponse = await mockRequest({
+    path: '/api/movies',
+    action: 'get',
+  });
   expect(moviesResponse.status).toBe(200);
   expect(moviesResponse.body.length).toBe(ratedMovieList.length);
 
@@ -62,7 +64,10 @@ test('topfive', async () => {
   //Mad Max: Fury Road does not have any rating
 
   //Get the top five movies
-  const topFiveResponse = await request(app).get('/api/movies/topfive');
+  const topFiveResponse = await mockRequest({
+    path: '/api/movies/topfive',
+    action: 'get',
+  });
   expect(topFiveResponse.status).toBe(200);
   expect(topFiveResponse.body.length).toBe(5);
 
@@ -98,12 +103,14 @@ test('topfive', async () => {
 
 async function rateMovie(ratings: number[], movieTitle: string) {
   for (const rating of ratings) {
-    await request(app)
-      .post('/api/movies/reviews')
-      .send({
+    await mockRequest({
+      path: '/api/movies/reviews',
+      action: 'post',
+      body: {
         movieId: ratedMovieList.find((movie) => movie.title === movieTitle)
           ?.movieId,
         rating: rating,
-      });
+      },
+    });
   }
 }
